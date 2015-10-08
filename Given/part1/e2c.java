dca05a727f1eeab406aa8efc37659467f285a4a3
Charles Chen <cyuchen@ucdavis.edu>
2015-10-07 21:49:59 -0700

    update

--- Given/part1/e2c.java
+++ Given/part1/e2c.java
@@ -1,11 +1,11 @@
 public class e2c {
  
-    public static void main(String args[]) {
-	Scan scanner = new Scan();
-	Token t;
-	do {
+  public static void main(String args[]) {
+	  Scan scanner = new Scan();
+	  Token t;
+	  do {
 	    t = scanner.scan();
 	    System.out.println(t);
-	} while( t.kind != TK.EOF );	     
-    }
+	  } while( t.kind != TK.EOF );	     
+  }
 }
