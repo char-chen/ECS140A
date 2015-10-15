2fec8838eb5412d714acecf1e34eb96bad73e775
Charles Chen <cyuchen@ucdavis.edu>
2015-10-14 19:30:23 -0700

    part 2 almost done

--- HW2/part2/Parser.java
+++ HW2/part2/Parser.java
@@ -42,14 +42,108 @@ public class Parser {
 	    mustbe(TK.DECLARE);
 	    mustbe(TK.ID);
 	    while( is(TK.COMMA) ) {
-	    scan();
-	    mustbe(TK.ID);
+	      scan();
+	      mustbe(TK.ID);
 	    }
     }
 
     private void statement_list() {
+      while (statement());
+    }
+    
+    private boolean statement() {
+      if (is(TK.ID) || is(TK.TILDE))
+        assignment();
+      else if(is(TK.PRINT)) // !
+        print();
+      else if(is(TK.DO)) // <
+        DO();
+      else if(is(TK.IF)) // [
+        IF();
+      else
+        return false;
+      
+      return true;
+    }
+    
+    private void print() {
+      scan(); 
+      expr(); 
+    } 
+    
+    private void assignment() {
+      ref_id();
+      mustbe(TK.ASSIGN);
+      expr();
+    }
+    
+    private void ref_id() {
+      if (is(TK.TILDE))
+      {
+        scan();
+      
+        if (is(TK.NUM))
+          scan();
+      }
+
+      mustbe(TK.ID);
     }
 
+    private void DO() {
+      scan();
+      guarded_command();
+    }
+    
+    private void IF() {
+      scan();
+      guarded_command();
+    }
+    
+    private void guarded_command() {
+      expr();
+      mustbe(TK.THEN);
+      block();
+    }
+
+    private void expr() {
+      term();
+      
+      while (addop())
+        term();
+    }
+
+    private void term() {
+      factor();
+      
+      while (multop())
+        factor();
+    }
+    
+    private void factor() {
+      if (is(TK.LPAREN)) {
+        expr();
+        mustbe(TK.RPAREN);
+      }
+      else if (is(TK.ID))
+        ref_id();
+      else
+        mustbe(TK.NUM);    
+    }
+     
+    private boolean addop() {
+      if (is(TK.PLUS) || is(TK.MINUS))
+        return true;
+      
+      return false; 
+    }
+
+    private boolean multop() {
+      if (is(TK.TIMES) || is(TK.DIVIDE))
+        return true;
+  
+      return false;
+    }
+      
     // is current token what we want?
     private boolean is(TK tk) {
       return tk == tok.kind;
@@ -58,8 +152,8 @@ public class Parser {
     // ensure current token is tk and skip over it.
     private void mustbe(TK tk) {
 	    if( tok.kind != tk ) {
-	    System.err.println( "mustbe: want " + tk + ", got " + tok);
-	    parse_error( "missing token (mustbe)" );
+	      System.err.println( "mustbe: want " + tk + ", got " + tok);
+	      parse_error( "missing token (mustbe)" );
 	    }
       
 	    scan();
