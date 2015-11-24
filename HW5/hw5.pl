40e40c004307d2aff36f3d5f8ee541ae91570e26
itsmarky10 <markfan@ymail.com>
2015-11-23 22:12:35 -0800

    Finished part 1 a-c.
    I'll leave you guys to finish d-f. 
    Planning to start on part 2.

--- HW5/hw5.pl
+++ HW5/hw5.pl
@@ -2,4 +2,22 @@
 /**    Your solution goes in this file    **/ 
 /*******************************************/
 
+/*starting in part 1*/ 
+%a 
+
+fc_course(C) :- 
+    course(C, _, Units), 
+    (Units=3; Units=4). 
+    
+%b 
+prereq_110(C) :-
+    course(C, P, _),
+    member(ecs110, P). 
+    
+%c 
+ecs140a_students(Name) :- 
+    student(Name, Class), 
+    member(ecs140a, Class). 
+    
+%d
 
