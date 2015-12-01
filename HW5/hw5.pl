45c8a0de708646e7896ffabc5b4ac7d09eb6593c
Mark Fan <markfan@ymail.com>
2015-12-01 09:15:11 -0800

    Finished part 2 (a-c).

--- HW5/hw5.pl
+++ HW5/hw5.pl
@@ -53,3 +53,36 @@ prereq(Courses, Prereq) :-
   course(Courses, List, _),
   member(X, List),
   prereq(X, Prereq).
+
+%Starting at part 2
+%all_length
+all_length([],0). %base case for 0. 
+all_length([H|T], Len):- 
+    atom(H), 
+    all_length(T, LenT), 
+    Len is LenT + 1. 
+    
+all_length([[H|TH] | T], Len):- 
+    all_length([H|TH], LenT), 
+    all_length(T, LenT1), 
+    Len is LenT + LenT1. 
+    
+%equal_a_b 
+equal_a_b(L):- 
+    equal_help(L,0,0). 
+
+equal_help([], Acount, Acount). 
+
+equal_help([H|T], Acount, Bcount):- 
+    H = a -> (HA is Acount + 1, equal_help(T, HA, Bcount)); 
+    H = b -> (HB is Bcount + 1, equal_help(T, Acount, HB)); 
+    equal_help(T, Acount, Bcount). 
+
+%swap_prefix_suffix
+swap_prefix_suffix(K, L, S):- %K is is the middle position. L is what has been appended. S - swap
+    append3(Prefix, K, Suffix, L), 
+    append3(Suffix, K, Prefix, S). 
+    
+append3(L1, L2, L3, L):- 
+    append(L1, LL, L), 
+    append(L2,L3,LL). 
