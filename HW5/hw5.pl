be5c108b9fbea041886522f6912ce25573b7bd17
Charles Chen <cyuchen@ucdavis.edu>
2015-11-30 18:37:44 -0800

    Part 1 complete

--- HW5/hw5.pl
+++ HW5/hw5.pl
@@ -20,4 +20,36 @@ ecs140a_students(Name) :-
     member(ecs140a, Class). 
     
 %d
+instructor_names(Name) :-
+  instructor(Name, _),
+  teach_john(Name).
 
+teach_john(Name) :-
+  student(john, S_courses),
+  instructor(Name, I_courses),
+  member(Courses, I_courses), 
+  member(Courses, S_courses),
+  !.
+
+%e
+students(Name) :-
+  student(Name, _),
+  student_jim(Name).
+
+student_jim(Name) :-
+  instructor(jim, I_courses),
+  student(Name, S_courses),
+  member(Courses, I_courses),
+  member(Courses, S_courses),
+  !.
+
+%f
+allprereq(Courses, Prereq) :-
+  findall(CC, (course(CC, _, _), prereq(Courses, CC)), Prereq).
+
+prereq(Courses, Prereq) :-
+  course(Courses, List, _),
+  member(Prereq, List);
+  course(Courses, List, _),
+  member(X, List),
+  prereq(X, Prereq).
