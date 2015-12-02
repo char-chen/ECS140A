6e3d3ef654352237f88edd4a8a41c5478ca0b9ab
Charles Chen <cyuchen@ucdavis.edu>
2015-12-01 19:12:48 -0800

    style fix

--- HW5/hw5.pl
+++ HW5/hw5.pl
@@ -1,23 +1,18 @@
-/*******************************************/
-/**    Your solution goes in this file    **/ 
-/*******************************************/
-
-/*starting in part 1*/ 
+/* Part 1 */ 
 %a 
-
 fc_course(Cl) :- 
-    course(Cl, _, Units), 
-    (Units=3; Units=4). 
-    
+  course(Cl, _, Units), 
+  (Units=3; Units=4). 
+  
 %b 
 prereq_110(C) :-
-    course(C, P, What),
-    member(ecs110, P). 
+  course(C, P, What),
+  member(ecs110, P). 
     
 %c 
 ecs140a_students(Name) :- 
-    student(Name, Class), 
-    member(ecs140a, Class). 
+  student(Name, Class), 
+  member(ecs140a, Class). 
     
 %d
 instructor_names(Name) :-
@@ -54,35 +49,38 @@ prereq(Courses, Prereq) :-
   member(X, List),
   prereq(X, Prereq).
 
-%Starting at part 2
+/* Part 2 */
 %all_length
 all_length([],0). %base case for 0. 
 all_length([H|T], Len):- 
-    atom(H), 
-    all_length(T, LenT), 
-    Len is LenT + 1. 
+  atom(H), 
+  all_length(T, LenT), 
+  Len is LenT + 1. 
     
 all_length([[H|TH] | T], Len):- 
-    all_length([H|TH], LenT), 
-    all_length(T, LenT1), 
-    Len is LenT + LenT1. 
+  all_length([H|TH], LenT), 
+  all_length(T, LenT1), 
+  Len is LenT + LenT1. 
     
 %equal_a_b 
 equal_a_b(L):- 
-    equal_help(L,0,0). 
+  equal_help(L,0,0). 
 
 equal_help([], Acount, Acount). 
 
 equal_help([H|T], Acount, Bcount):- 
-    H = a -> (HA is Acount + 1, equal_help(T, HA, Bcount)); 
-    H = b -> (HB is Bcount + 1, equal_help(T, Acount, HB)); 
-    equal_help(T, Acount, Bcount). 
+  H = a -> (HA is Acount + 1, equal_help(T, HA, Bcount)); 
+  H = b -> (HB is Bcount + 1, equal_help(T, Acount, HB)); 
+  equal_help(T, Acount, Bcount). 
 
 %swap_prefix_suffix
 swap_prefix_suffix(K, L, S):- %K is is the middle position. L is what has been appended. S - swap
-    append3(Prefix, K, Suffix, L), 
-    append3(Suffix, K, Prefix, S). 
+  append3(Prefix, K, Suffix, L), 
+  append3(Suffix, K, Prefix, S). 
     
 append3(L1, L2, L3, L):- 
-    append(L1, LL, L), 
-    append(L2,L3,LL). 
+  append(L1, LL, L), 
+  append(L2,L3,LL).
+
+palin([]) :-
+    
